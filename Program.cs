fe2cc4837e49117f0db9c1b88b7f3c215b08d52d
Samuel Martins <samukamartins007@gmail.com>
2019-08-31 19:20:21 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Course3.Entities;
+using System.Globalization;
+
+namespace Course3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>();
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual Income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healtExpenditure = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualIncome, healtExpenditure));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            double som = 0;
+            foreach (TaxPayer obj in list)
+            {
+                som += obj.tax();
+                Console.WriteLine(obj.Name + ": $ " + obj.tax().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.WriteLine();
+            Console.Write("TOTAL TAXES: $ "+som);
+        }
+    }
+}
